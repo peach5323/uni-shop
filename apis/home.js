5a84e8075deaa2297084f5a86499bc325ec6baf8
peach <peach5323@hotmail.com>
2024-04-11 21:45:14 +0800

    完成home首页

--- apis/home.js
+++ apis/home.js
@@ -1,7 +1,22 @@
 import api from '../utils/request.js'
+// 获取轮播图
 export function getSwiperData(){
   return api.request({
     url:'/api/public/v1/home/swiperdata',
+    method:'get'
+  })
+}
+// 获取分类导航
+export function getNavData(){
+  return api.request({
+    url:'/api/public/v1/home/catitems',
+    method:'get'
+  })
+}
+// 获取楼层数据
+export function getFloorData(){
+  return api.request({
+    url:'/api/public/v1/home/floordata',
     method:'get'
   })
 }
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,25 +8,70 @@
         </navigator>
       </swiper-item>
     </swiper>
+    <view class="nav-list">
+      <view v-for="(item,i) in navList" :key="i" class="nav-item" @click="navClickHandler(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+    <view class="floor-list">
+      <view class="floot-item" v-for="(item,i) in floorList" :key="i">
+        <image class="floot-title" :src="item.floor_title.image_src"></image>
+        <view class="floor-products">
+          <navigator class="left-img" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+          </navigator>
+          <view class="right-img">
+            <navigator class="right-img-item" v-for="(item2,i2) in item.product_list" :key="i2" v-if="i2!==0" :url="item2.url">
+              <image :src="item2.image_src" :style="{width: item2.image_width + 'rpx'}" mode="widthFix"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+      
+    </view>
   </view>
 </template>
 
 <script>
-  import {getSwiperData} from '../../apis/home'
+  import {getSwiperData,getNavData,getFloorData} from '../../apis/home'
   export default {
     data() {
       return {
-        swiperList:[]
+        swiperList:[],
+        navList:[],
+        floorList:[]
       };
     },
     onLoad() {
       this.getSwiperList()
+      this.getNavList()
+      this.getFloorList()
     },
     methods:{
       async getSwiperList(){
        const {message:res} = await getSwiperData()
-       this.swiperList=res
-       // console.log(res);
+       this.swiperList = res
+      },
+      async getNavList(){
+       const {message:res} = await getNavData()
+       this.navList = res
+      },
+      async getFloorList(){
+       const {message:res} = await getFloorData()
+       // 处理数据
+       res.forEach(item=>{
+         item.product_list.forEach(i=>{
+          i.url = '/subpkg/goods_list/goods_list?'+i.navigator_url.split('?')[1]
+         })
+       })
+       this.floorList = res
+      },
+      navClickHandler(item){
+        if(item.name==='分类'){
+          uni.switchTab({
+            url:'/pages/cate/cate'
+          })
+        }
       }
     }
   }
@@ -39,6 +84,30 @@ swiper{
   image{
     width: 100%;
     height: 100%;
+  }
+}
+.nav-list{
+  display: flex;
+  justify-content: space-around;
+  margin: 30rpx 0;
+  .nav-img{
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
+.floot-title{
+  height: 60rpx;
+  width: 100%;
+}
+.floor-products{
+  display: flex;
+  .left-img{
+    padding-left: 10rpx;
+  }
+  .right-img{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
   }
 }
 </style>
